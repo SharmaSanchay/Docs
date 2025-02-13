64d17b06ae4db27402791a0dd549b1b480a665bb
Sanchay Sharma <[sanchaysharm18@gmail.com]>
2025-02-13 20:35:37 +0530

    Wrapped

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+require('dotenv').config()
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -7,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDPSdUqoWooPeFHkR9Jed4zBjJwoj_4w68",
-  authDomain: "docs-clone-bd45c.firebaseapp.com",
-  projectId: "docs-clone-bd45c",
-  storageBucket: "docs-clone-bd45c.firebasestorage.app",
-  messagingSenderId: "636768006323",
-  appId: "1:636768006323:web:593e91ca5f1ce3de2ac0c6"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket:process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 // Initialize Firebase
